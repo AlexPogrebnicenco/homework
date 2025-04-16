61a782bcd9121600bf9f95ea48bb5445ba3dee6c
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-04-16 12:35:03 +0300

    Generics examples

--- HomeworkProject/HomeworkProject/Program.cs
+++ HomeworkProject/HomeworkProject/Program.cs
@@ -1,17 +1,106 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeworkProject
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Homework 6 has been created");
-            Console.ReadLine(); 
+           Plant grass = new Plant { Name = "Grass"};
+            Herbivore antelope = new Herbivore();
+            Predator<Herbivore> lion = new Predator<Herbivore>();
+
+            antelope.Eat(grass);
+            //antelope.Eat(lion);
+            lion.Eat(antelope);
+
+            Farmer farmer = new Farmer();
+            farmer.Feed(lion, antelope);
+            farmer.Feed(antelope, grass);
+
+            farmer.Feed<Herbivore, Plant>(antelope);
+
+            Mouse mouse = new Mouse();
+            //farmer.Feed<Mouse, Cheese>(mouse);
+            farmer.Feed<Mouse, Cheese>(mouse, new Cheese(""));
+
+
+            Console.ReadLine();
+            
+        }
+
+        public interface ILivingBeing
+        {
+            string Name { get; set; }
+        }
+
+        public class Cheese
+        {
+            public string Kind { get;}
+            public Cheese(string kind)
+            {
+                Kind = kind;
+            } 
+        }
+
+        public class Mouse : Animal<Cheese>
+        {
+        }
+
+        public class Plant : ILivingBeing
+        {
+            public string Name { get; set; }
+            public int Height { get; private set; }
+            public void Grow()
+            {
+                Console.WriteLine("Growing");
+                Height++;
+            }
+
+            public override string ToString()
+            {
+                return Name ?? "Unknown plant";
+            }
+        }
+
+        public class Animal<K> : ILivingBeing where K : class
+        {
+            public string Name { get; set;}
+            public int Weight { get; set; }
+            public void Eat(K food)
+            {
+                Console.WriteLine($"Eating {food}");
+            }
+        }
+
+        public class Herbivore : Animal<Plant>
+        {
+        }
+
+        public class Predator<K> : Animal<K> where K : Herbivore
+        { 
+        }
+
+        public class Farmer
+        {
+            public void Feed<T, K>(T animal, K food) where T : Animal<K> where K: class
+            {
+                Console.WriteLine($"Feeding {animal} with {food}");
+            }
+
+            public void Feed<T, K>(T animal) where T : Animal<K> where K : class, new()
+            {
+                Console.WriteLine("Preparing the food");
+                K food = new K();
+                Console.WriteLine($"Feeding {animal} with {food}");
+
+            }
         }
     }
 }
