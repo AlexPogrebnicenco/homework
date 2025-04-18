f2b78a1a73b10582afaa395bbe040182f26e4bb3
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-04-18 14:59:51 +0300

    hw7 is ready for mentor review

--- HomeworkProject/HomeworkProject/Program.cs
+++ HomeworkProject/HomeworkProject/Program.cs
@@ -1,17 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using HomeworkProject.Exceptions;
 
 namespace HomeworkProject
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hw7 has been created");
-            Console.ReadLine(); 
+            Console.WriteLine("===Registration Start===");
+#if DEBUG
+            Console.WriteLine("DEBUG Program is working in debug mode");
+#endif
+            UserService.RegisterUser("Alexandru", 27);
+            Console.WriteLine("===Registration End===");
+
+            //Console.WriteLine("===Registration Start===");
+            //UserService.RegisterUser("", -5);
+            //Console.WriteLine("===Registration End===");
+
+            Console.WriteLine("===Testing ParseAge===");
+
+            try
+            {
+                //string name = "";                                               // --- Uncomment for Testing ParseAge
+                //string input = "abcd";                                          // --- Uncomment for Testing ParseAge
+                //int age = UserService.ParseAge(input);                          // --- Uncomment for Testing ParseAge
+                //UserService.RegisterUser(name, age);                            // --- Uncomment for Testing ParseAge
+                //Console.WriteLine($"Age was successfully transformed: {age}");  // --- Uncomment for Testing ParseAge
+
+                // Testing HandleRegistration 
+                UserService.HandleRegistration("", "efgh");
+            }
+            catch (InvalidAgeException iae)
+            {
+                Console.WriteLine($"Age exception: {iae.Message}");
+                Console.WriteLine($"Original exception: {iae.InnerException?.Message}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Argument exception: {ae.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally worked");
+            }
+
         }
     }
 }
---
+++ HomeworkProject/HomeworkProject/UserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeworkProject.Exceptions;
+
+namespace HomeworkProject
+{
+    public static class UserService
+    {
+        public static void RegisterUser(string name, int age)
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name can not be empty or a space.", nameof(name));
+            }
+
+            if (age < 0 || age > 120)
+            {
+                throw new ArgumentOutOfRangeException(nameof(age), "Age must be between 0 and 120.");
+            }
+
+            
+            Console.WriteLine($"User Registration: {name}, age: {age}");
+        }
+
+        public static int ParseAge(string input) 
+        {
+            try
+            {
+                return int.Parse(input);
+            }
+            catch (FormatException ex)
+            {
+                throw new InvalidAgeException("Error in changing age. The format is invalid", ex);
+            }
+        }
+
+        public static void HandleRegistration(string name, string inputAge)
+        {
+            try
+            {
+                int age = ParseAge(inputAge);
+                RegisterUser(name, age);
+            }
+            catch (InvalidAgeException iae)
+            {
+                Console.WriteLine($"Age error from HandleRegistration: {iae.Message}");
+                throw;
+            }
+        }
+    }
+}
