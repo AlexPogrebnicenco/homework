b24efca2bec9129ef3e02d0c4b2bf29df4a19ee0
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-04-18 11:54:36 +0300

    hw7 has been created

--- HomeworkProject/HomeworkProject/Program.cs
+++ HomeworkProject/HomeworkProject/Program.cs
@@ -10,7 +10,7 @@ namespace HomeworkProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hw7 has been created");
             Console.ReadLine(); 
         }
     }
