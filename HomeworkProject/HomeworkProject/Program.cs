01da05e2eb0e34b5824bebb77a9d0ee73fefd8c5
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-04-22 12:13:04 +0300

    hw10 is ready for mentors review

--- HomeworkProject/HomeworkProject/Program.cs
+++ HomeworkProject/HomeworkProject/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace HomeworkProject
 {
@@ -10,8 +12,33 @@ namespace HomeworkProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hw10 has been created");
-            Console.ReadLine(); 
+            Console.Write("Write your email:");
+            string email = Console.ReadLine();
+            Console.WriteLine($"You wrote: {email}");
+
+            try
+            {
+                using (MailMessage message = new MailMessage())
+                {
+                    message.From = new MailAddress("pogrebnicencoalex@gmail.com");
+                    message.To.Add(email);
+                    message.Subject = "Thank you for subscribing!";
+                    message.Body = "You have successfully subscribed to our Onlince-Course-App";
+
+                    using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        client.Credentials = new NetworkCredential("pogrebnicencoalex@gmail.com", "************");
+                        client.EnableSsl = true;
+                        client.Send(message);
+                    }
+
+                    Console.WriteLine("Message was sent");
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Houston, we have a problem: " + ex.Message);
+            }
         }
     }
 }
