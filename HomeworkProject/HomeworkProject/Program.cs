ff7cdf964df59360bdb83ed4e19e451d6dcad7c2
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-04-25 17:45:15 +0300

    hw14 has been created

--- HomeworkProject/HomeworkProject/Program.cs
+++ HomeworkProject/HomeworkProject/Program.cs
@@ -10,7 +10,7 @@ namespace HomeworkProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hw14 has been created");
             Console.ReadLine(); 
         }
     }
